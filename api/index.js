d24df372074892df0ac17b7d112e66811c5b1369
Dario Ponce <daroponce21@gmail.com>
2023-01-30 16:44:31 -0300

    probando1

--- api/index.js
+++ api/index.js
@@ -4,8 +4,6 @@ const { PORT } = process.env;
 
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log('Escuchando el puerto ', PORT); 
+    console.log("%s listening at 3000"); // eslint-disable-line no-console
   });
 });
-
-
