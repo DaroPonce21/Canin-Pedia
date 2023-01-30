2c7fd7e3b0606e6ae27df5b22ecc4af9a9479ded
Dario Ponce <daroponce21@gmail.com>
2023-01-30 11:55:17 -0300

    arreglo deploy

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const { PORT } = process.env;
 
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log('%s listening at 3001'); 
+    console.log('Escuchando el puerto ', PORT); 
   });
 });
 
